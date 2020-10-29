56f257ad7bd810687511c7bd2b6957297c27e014
Tarso José <tarsojf@gmail.com>
2020-10-29 11:39:44 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php 	
 
 use \Hcode\Page;
+use \Hcode\Model\Products;
 
 $app->get('/', function() {
+
+	$products = Products::listAll();
     
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Products::checkList($products)
+	]);
 
 });
 
