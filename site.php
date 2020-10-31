78d199b65cad6fb0358c7e18c207115037e5a9ad
Tarso José <tarsojf@gmail.com>
2020-10-31 15:02:55 -0300

    Detalhes do produto

--- site.php
+++ site.php
@@ -19,15 +19,45 @@ $app->get('/', function() {
 
 $app->get('/category/:idcategory', function($idcategory){
 
+	$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination["pages"]; $i++) { 
+		array_push($pages, [
+			"link"=>"/category/".$category->getidcategory()."?page=".$i,
+			"page"=>$i
+		]);
+	}
 
 	$page = new Page();
 
 	$page->setTpl("category", [
 		"category"=>$category->getValues(),
-		"products"=>Products::checkList($category->getproducts())
+		"products"=>$pagination["data"],
+		"pages"=>$pages
+	]);
+
+});
+
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Products();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
 	]);
 
 });
